76e8578e6ad5d4205fabe10b7904da0cb5ca8534
Soren Marcelino <soren.marcelino@icloud.com>
2023-10-08 15:31:16 +0200

    Initial Commit

---
+++ Neros Music/Neros_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neros_MusicApp.swift
+//  Neros Music
+//
+//  Created by Soren Marcelino on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Neros_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
