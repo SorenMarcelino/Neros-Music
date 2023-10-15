e3a381992b145ad5d77d9f28440ac9d188a0c3ed
Soren Marcelino <soren.marcelino@icloud.com>
2023-10-15 16:01:24 +0200

    Memoji visible on second page

---
+++ Neros Music/View/CustomSceneView.swift
@@ -0,0 +1,8 @@
+//
+//  CustomSceneView.swift
+//  Neros Music
+//
+//  Created by Soren Marcelino on 15/10/2023.
+//
+
+import Foundation
---
+++ Neros Music/View/MemojiView.swift
@@ -0,0 +1,16 @@
+//
+//  MemojiView.swift
+//  Neros Music
+//
+//  Created by Soren Marcelino on 15/10/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct MemojiView: View {
+    var body: some View {
+        Text("Ceci est la dexième vue")
+            .font(.largeTitle)
+    }
+}
